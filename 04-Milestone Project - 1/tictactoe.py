4ee846823ce270d41695ae92ce936c9a1dc5e682
Auric Goldfinger <goldfinger.auric@gmail.com>
2020-05-22 21:05:48 +0200

    Set up board and read user input

---
+++ 04-Milestone Project - 1/tictactoe.py
@@ -0,0 +1,48 @@
+import math
+
+board = [['' for j in range(0,3)] for i in range(0,3)]
+debug = True
+
+def print_debug(s) :
+    if (debug) : print(s)
+
+def print_board() :
+    global board
+    title = "Current board\n"
+    for c in title:
+        title += "="
+    
+    print(f"\n{title}\n")
+
+    for i, row in enumerate(board):
+        print(f"\t{row[0]:^3}|{row[1]:^3}|{row[2]:^3}")
+        if i < 2 :
+            print(f"\t-----------")
+
+    print('')
+
+def read_user_input() :
+    global board
+    ask_for_input = True
+    while ask_for_input :
+        yourinput = input("Feed me a number from 1 to 9: ")
+        if yourinput == 'q':
+            print("Thank you for playing!")
+            exit()
+        elif (not yourinput.isdigit()) :
+            print("Oh come on.")
+            continue
+
+        yournumber = int(yourinput)-1
+
+        if yournumber > 8 or yournumber < 0:
+            print("Number out of range.")
+            continue
+
+        row = int(math.floor(yournumber / 3))
+        col = yournumber % 3
+        print_debug(f"row: {row}, col: {col}")
+        board[row][col] = yourinput
+        print_board()
+
+read_user_input()
