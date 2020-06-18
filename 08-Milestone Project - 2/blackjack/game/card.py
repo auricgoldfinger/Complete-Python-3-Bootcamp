78ab3a6f26be8c0ca362684debe284648668c7c7
Auric Goldfinger <goldfinger.auric@gmail.com>
2020-06-18 22:09:05 +0200

    Add card representation

--- 08-Milestone Project - 2/blackjack/game/card.py
+++ 08-Milestone Project - 2/blackjack/game/card.py
@@ -6,6 +6,9 @@ class Card:
 
     def __str__(self):
         return f"{self.rank.name} of {self.suit.name}"
+    
+    def __repr__(self):
+        return str(self)
 
     def value(self):
         return self.rank.value
