fbfa929d3edf1008fbf37e8b2b1e1214149b945f
Auric Goldfinger <goldfinger.auric@gmail.com>
2020-06-18 20:13:25 +0200

    Errors and exceptions notes

---
+++ 07-Errors and Exception Handling/notes/cap.py
@@ -0,0 +1,9 @@
+def cap_text(text):
+    '''
+    Input a string
+    Output the capitalized text
+    '''
+    return text.title()
+
+if __name__ == '__main__':
+    print(cap_text("called by main execution"))
