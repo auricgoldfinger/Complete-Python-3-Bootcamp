d5f6e3d173922ee1d8528230dd2f9836f87dcab7
Auric Goldfinger <goldfinger.auric@gmail.com>
2020-06-18 22:10:20 +0200

    Give values to ranks

--- 08-Milestone Project - 2/blackjack/game/blackjack.py
+++ 08-Milestone Project - 2/blackjack/game/blackjack.py
@@ -1,4 +1,14 @@
-from rank import Rank
-from suit import Suit
-rank = Rank.ACE
-print(rank.value)
+from game.rank import Rank
+from game.suit import Suit
+
+rank_values = {}
+for rank in Rank:
+    rank_value = rank.value
+    if Rank.ACE == rank:
+        rank_value = 11
+    elif Rank.JACK == rank or Rank.QUEEN == rank or Rank.KING == rank:
+        rank_value = 10
+    
+    rank_values[rank] = rank_value
+
+print(rank_values)
