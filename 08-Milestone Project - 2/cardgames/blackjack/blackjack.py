2bf3a33ab6730736dee95411e7cc129b9772c275
Auric Goldfinger <goldfinger.auric@gmail.com>
2020-06-19 22:29:02 +0200

    Add possibility to place bet

--- 08-Milestone Project - 2/cardgames/blackjack/blackjack.py
+++ 08-Milestone Project - 2/cardgames/blackjack/blackjack.py
@@ -1,9 +1,22 @@
 from game.rank import Rank
 from game.suit import Suit
-from blackjack.utils import BlackjackUtils
+from game.player import Player
+from utils import BlackjackUtils
 
-rank_values = {}
-for rank in Rank:
-    rank_values[rank] = BlackjackUtils.rankEvaluator(rank)
+dealer = Player("dealer", BlackjackUtils.calculate_value)
+human_player = Player("player", BlackjackUtils.calculate_value)
 
-print(rank_values)
+def take_bet():
+
+    is_bet_placed = False
+    while not is_bet_placed: 
+        try:
+            bet = input("Place your bet: ")
+            human_player.chips.place_bet(int(bet))
+            is_bet_placed = True
+        except IndexError as e:
+            print(e.args[0])
+        except:
+            print("That's not a valid bet.")
+
+take_bet()
