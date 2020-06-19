54015709d16daba9a8725d79bc54da0f64cc5461
Auric Goldfinger <goldfinger.auric@gmail.com>
2020-06-19 22:14:49 +0200

    Introduce script to run all tests together

---
+++ 08-Milestone Project - 2/cardgames/test/all_tests.py
@@ -0,0 +1,17 @@
+import glob
+import unittest
+import os
+
+def create_test_suite():
+    prefix = "."+os.sep+"08-Milestone Project - 2"+os.sep+"cardgames"+os.sep+"test"+os.sep
+    test_file_strings = glob.glob(prefix+"test_*.py")
+    print(f"test file strings = {test_file_strings}")
+
+    module_strings = ['test.'+str[len(prefix):len(str)-3] for str in test_file_strings]
+    print(f"module strings = {module_strings}")
+
+    suites = [unittest.defaultTestLoader.loadTestsFromName(name) for name in module_strings]
+    print(f"suites = {suites}")
+
+    testSuite = unittest.TestSuite(suites)
+    return testSuite
