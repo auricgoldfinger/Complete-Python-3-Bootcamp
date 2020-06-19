6ec4276c7a5d9e7060a3b286674156071a7ad4d9
Auric Goldfinger <goldfinger.auric@gmail.com>
2020-06-19 22:35:21 +0200

    Add function to hit

--- 08-Milestone Project - 2/cardgames/blackjack/blackjack.py
+++ 08-Milestone Project - 2/cardgames/blackjack/blackjack.py
@@ -1,10 +1,12 @@
 from game.rank import Rank
 from game.suit import Suit
+from game.deck import Deck
 from game.player import Player
 from utils import BlackjackUtils
 
 dealer = Player("dealer", BlackjackUtils.calculate_value)
 human_player = Player("player", BlackjackUtils.calculate_value)
+deck = Deck()
 
 def take_bet():
 
@@ -19,4 +21,15 @@ def take_bet():
         except:
             print("That's not a valid bet.")
 
+def hit(deck,hand):
+    hand.add_card(deck.deal())
+    if hand.value() > 21:
+        print("Busted")
+    elif hand.value() == 21:
+        print("Won!")
+    else:
+        print("Continue")
+
 take_bet()
+deck.shuffle()
+hit(deck, human_player.hand)
