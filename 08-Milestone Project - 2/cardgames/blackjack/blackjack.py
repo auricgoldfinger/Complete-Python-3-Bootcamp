a43906a4cd176d7f33091488549098dc27e2cac0
Auric Goldfinger <goldfinger.auric@gmail.com>
2020-06-19 22:45:08 +0200

    Add hit_or_stand method

--- 08-Milestone Project - 2/cardgames/blackjack/blackjack.py
+++ 08-Milestone Project - 2/cardgames/blackjack/blackjack.py
@@ -4,6 +4,8 @@ from game.deck import Deck
 from game.player import Player
 from utils import BlackjackUtils
 
+playing = True
+
 dealer = Player("dealer", BlackjackUtils.calculate_value)
 human_player = Player("player", BlackjackUtils.calculate_value)
 deck = Deck()
@@ -23,13 +25,25 @@ def take_bet():
 
 def hit(deck,hand):
     hand.add_card(deck.deal())
-    if hand.value() > 21:
+    value = hand.value()
+    print(f"Holding {hand.cards}  ({value})")
+    if value > 21:
         print("Busted")
-    elif hand.value() == 21:
+    elif value == 21:
         print("Won!")
     else:
         print("Continue")
 
+def hit_or_stand(deck,hand):
+    global playing  # to control an upcoming while loop
+    
+    while playing and hand.value() < 21:
+        next_step = input("Hit or stand? ")
+        if "hit" == next_step.lower():
+            hit(deck, hand)
+        else:
+            break
+
 take_bet()
 deck.shuffle()
-hit(deck, human_player.hand)
+hit_or_stand(deck, human_player.hand)
