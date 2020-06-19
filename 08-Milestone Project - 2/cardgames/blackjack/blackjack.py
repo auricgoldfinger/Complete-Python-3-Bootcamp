c509ca751af669b7ff39e6ca7cdc0c673cc75365
Auric Goldfinger <goldfinger.auric@gmail.com>
2020-06-19 23:04:30 +0200

    Prepare for predefined gameplay

--- 08-Milestone Project - 2/cardgames/blackjack/blackjack.py
+++ 08-Milestone Project - 2/cardgames/blackjack/blackjack.py
@@ -6,9 +6,6 @@ from utils import BlackjackUtils
 
 playing = True
 
-dealer = Player("dealer", BlackjackUtils.calculate_value)
-human_player = Player("player", BlackjackUtils.calculate_value)
-deck = Deck()
 
 def take_bet():
 
@@ -52,22 +49,60 @@ def prepare(deck, player):
     hit(deck,player.hand)
     hit(deck,player.hand)
 
-show_some(dealer)
-show_all(human_player)
 
-print("Shuffling...")
-deck.shuffle()
-print("Prepare dealer...")
-prepare(deck,dealer)
-print("Prepare player...")
-prepare(deck,human_player)
+while True:
+    # Print an opening statement
+    print("BLACKJACK light")
+    dealer = Player("dealer", BlackjackUtils.calculate_value)
+    human_player = Player("player", BlackjackUtils.calculate_value)
 
-show_some(dealer)
-show_all(human_player)
+    show_some(dealer)
+    show_all(human_player)
+    
+    # Create & shuffle the deck, deal two cards to each player
+    deck = Deck()
+    print("Shuffling...")
+    deck.shuffle()
 
-take_bet()
+    print("Prepare dealer...")
+    prepare(deck,dealer)
 
-hit_or_stand(deck, human_player.hand)
+    print("Prepare player...")
+    prepare(deck,human_player)
+    
+    # Set up the Player's chips
+    # Prompt the Player for their bet
+    take_bet()
 
-show_all(dealer)
-show_all(human_player)
+    # Show cards (but keep one dealer card hidden)
+    show_some(dealer)
+    show_all(human_player)
+
+    
+    while playing:  # recall this variable from our hit_or_stand function
+        
+        # Prompt for Player to Hit or Stand
+        hit_or_stand(deck, human_player.hand)
+        
+        # Show cards (but keep one dealer card hidden)
+        show_all(dealer)
+        show_all(human_player)
+        
+        # If player's hand exceeds 21, run player_busts() and break out of loop
+        
+
+        break
+
+    # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
+    
+    
+    # Show all cards
+    
+    # Run different winning scenarios
+        
+    
+    # Inform Player of their chips total 
+    
+    # Ask to play again
+
+    break
